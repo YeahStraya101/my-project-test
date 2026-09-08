73e4c16225693c5a53bfbaf9d45b2b46088b79cc
YeahStraya101 <eliCARMOD@tkis.qld.edu.au>
2026-09-08 19:10:50 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-javascriptconst express = require('express');
-const Unblocker = require('unblocker');
+const express = require('express');
 const app = express();
+const Corrosion = require('corrosion');
 
-const unblocker = new Unblocker({prefix: '/proxy/'});
-app.use(unblocker);
+const proxy = new Corrosion({
+    prefix: '/proxy/',
+    codec: 'xor'
+});
+
+app.use((req, res, next) => {
+    if (req.url.startsWith('/proxy/')) {
+        return proxy.request(req, res);
+    }
+    res.send('<h1>Your Proxy Server is Live!</h1><p>Add <b>/proxy/https://spotify.com</b> to the end of the URL to browse.</p>');
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Proxy running on port ${port}`);
-}).on('upgrade', unblocker.onUpgrade);
+    console.log(`Server running on port ${port}`);
+});
